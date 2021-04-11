a29feedbeed8a0eecb86d7a3f6d0ff2213f4fa9c
Chay Carnell <chay.carnell@ibm.com>
2021-04-11 19:14:35 +0100

    update husky scripts

---
+++ babel.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@root': './src',
+          '@queries': './src/queries',
+          '@screens': './src/screens',
+          '@components': './src/components',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
+};
