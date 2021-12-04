5c6e1754f172ad08bd91b9a7b22254bcd64401d4
Chay Carnell <chay.carnell@ibm.com>
2021-12-04 18:06:38 +0000

    bump to 0.66.3

--- src/components/ApolloProvider/ApolloProvider.tsx
+++ src/components/ApolloProvider/ApolloProvider.tsx
@@ -1,3 +1,4 @@
+import 'cross-fetch/polyfill';
 import React from 'react';
 import {
   ApolloProvider,
@@ -20,70 +21,66 @@ interface Props {
 }
 
 const Render = ({ children }: Props) => {
-  try {
-    // Handle subscription operations
-    const hasSubscriptionOperation = ({ query: { definitions } }: any) =>
-      definitions.some(
-        ({ kind, operation }: any) =>
-          kind === 'OperationDefinition' && operation === 'subscription',
-      );
-
-    // Handle errors client side
-    const errorLink = onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors)
-        graphQLErrors.forEach(({ message, locations, path, extensions }) =>
-          console.log(
-            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}, Extension Code: ${
-              extensions && extensions.code
-            }`,
-          ),
-        );
-      if (networkError) console.log(`[Network error]: ${networkError}`);
-    });
-
-    // Attempt to get auth token from identity provider here i.e. getJwtToken()
-    const getTokenSilently = async () => null;
-
-    // Create authentication middleware to add id_token to requests
-    const authLink = setContext(async (_, { headers }) => ({
-      headers: {
-        ...headers,
-        authorization: (await getTokenSilently()) || '',
-      },
-    }));
-
-    // Get server URL segments
-    const hostSegments = new URL(serviceURL);
-    const wsProtocol = hostSegments.protocol === 'https:' ? 'wss' : 'ws';
-
-    // Create HTTP and WS link
-    const link = ApolloLink.split(
-      hasSubscriptionOperation,
-      new WebSocketLink(
-        new SubscriptionClient(`${wsProtocol}://${hostSegments.host}/graphql`, {
-          reconnect: true,
-          reconnectionAttempts: 5,
-          timeout: 30000,
-          connectionParams: async () => ({
-            authToken: await getTokenSilently(),
-          }),
-        }),
-      ),
-      createHttpLink({
-        uri: `${serviceURL}/graphql`,
-      }),
+  // Handle subscription operations
+  const hasSubscriptionOperation = ({ query: { definitions } }: any) =>
+    definitions.some(
+      ({ kind, operation }: any) =>
+        kind === 'OperationDefinition' && operation === 'subscription',
     );
 
-    // Create new apollo client
-    const apolloClient = new ApolloClient({
-      link: ApolloLink.from([errorLink, authLink, link]),
-      cache: new InMemoryCache(),
-    });
+  // Handle errors client side
+  const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors)
+      graphQLErrors.forEach(({ message, locations, path, extensions }) =>
+        console.log(
+          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}, Extension Code: ${
+            extensions && extensions.code
+          }`,
+        ),
+      );
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+  });
 
-    return <ApolloProvider client={apolloClient}>{children}</ApolloProvider>;
-  } catch {
-    return null;
-  }
+  // Attempt to get auth token from identity provider here i.e. getJwtToken()
+  const getTokenSilently = async () => Promise.resolve('');
+
+  // Create authentication middleware to add id_token to requests
+  const authLink = setContext(async (_, { headers }) => ({
+    headers: {
+      ...headers,
+      authorization: await getTokenSilently(),
+    },
+  }));
+
+  // Get server URL segments
+  const hostSegments = new URL(serviceURL);
+  const wsProtocol = hostSegments.protocol === 'https:' ? 'wss' : 'ws';
+
+  // Create HTTP and WS link
+  const link = ApolloLink.split(
+    hasSubscriptionOperation,
+    new WebSocketLink(
+      new SubscriptionClient(`${wsProtocol}://${hostSegments.host}/graphql`, {
+        reconnect: true,
+        reconnectionAttempts: 5,
+        timeout: 30000,
+        connectionParams: async () => ({
+          authToken: await getTokenSilently(),
+        }),
+      }),
+    ),
+    createHttpLink({
+      uri: `${serviceURL}/graphql`,
+    }),
+  );
+
+  // Create new apollo client
+  const apolloClient = new ApolloClient({
+    link: ApolloLink.from([errorLink, authLink, link]),
+    cache: new InMemoryCache(),
+  });
+
+  return <ApolloProvider client={apolloClient}>{children}</ApolloProvider>;
 };
 
 export default Render;
--- src/queries/queries.ts
+++ src/queries/queries.ts
@@ -1,4 +1,4 @@
-import gql from 'graphql-tag';
+import { gql } from '@apollo/client';
 
 export const SOME_QUERY = gql`
   query someQuery($someId: String) {
