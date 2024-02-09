e94598e470c8e77a64a005e71b3d22f318036be6
Chay Carnell <chaycarnell@gmail.com>
2024-02-09 21:54:44 +1000

    test(jest): update screen tests and import sort linting

--- src/App.test.js
+++ src/App.test.js
@@ -2,10 +2,12 @@
  * @format
  */
 
-import React from 'react';
 import 'react-native';
+
 // Note: test renderer must be required after react-native.
 import { render } from '@testing-library/react-native';
+import React from 'react';
+
 import App from './App';
 
 it('renders correctly', () => {
--- src/screens/Secondary/Secondary.test.js
+++ src/screens/Secondary/Secondary.test.js
@@ -1,33 +1,23 @@
-import { NavigationContext } from '@react-navigation/native';
 import { cleanup, fireEvent, render } from '@testing-library/react-native';
 import { ScreenNames } from '@types';
 import * as NavModule from '@utils/navigation';
 import React from 'react';
 
-import DashboardScreen from './Dashboard';
+import AuthScreen from './Secondary';
 
 afterEach(cleanup);
 
-describe('Dashboard screen', () => {
+describe('Secondary screen', () => {
   describe('given default props', () => {
     let navSpy;
     let renderedComponent;
 
-    const navContext = {
-      isFocused: () => true,
-      addListener: jest.fn(() => jest.fn()),
-    };
-
     beforeEach(() => {
       navSpy = jest.spyOn(NavModule, 'navigate').mockImplementation(jest.fn);
-      renderedComponent = render(
-        <NavigationContext.Provider value={navContext}>
-          <DashboardScreen />
-        </NavigationContext.Provider>,
-      );
+      renderedComponent = render(<AuthScreen />);
     });
 
-    afterEach(() => {
+    afterAll(() => {
       navSpy.mockRestore();
     });
 
@@ -38,8 +28,11 @@ describe('Dashboard screen', () => {
 
     it('should navigate to dashboard page when touchable highlight is selected', () => {
       const { getByTestId } = renderedComponent;
-      fireEvent.press(getByTestId('DashboardNavigationButton'));
-      expect(NavModule.navigate).toHaveBeenCalledWith(ScreenNames.SECONDARY);
+      fireEvent.press(getByTestId('SecondaryNavigationButton'));
+      expect(NavModule.navigate).toHaveBeenCalledWith(
+        ScreenNames.DASHBOARD,
+        {},
+      );
     });
   });
 });
