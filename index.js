aee534312d8916666fb01146e8bbd5d8a5c43532
Chay Carnell <chaycarnell@gmail.com>
2024-02-19 18:38:23 +1000

    fix(eas): fix bundle identifier reference in release builds

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import { AppRegistry } from 'react-native';
-import { name as appName } from './app.json';
+import { displayName as appName } from './app.json';
 import App from './src/App';
 import './src/translations';
 
